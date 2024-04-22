517b3e0103a53cff1df5663fd548b90d8590ac93
Guilherme <g.satilio@gmail.com>
2024-04-22 16:50:07 -0300

    add swich case e vetores

--- Trab-CalcIMC/Program.cs
+++ Trab-CalcIMC/Program.cs
@@ -1,19 +1,21 @@
 ﻿//6 - faça um programa que leia o peso, altura e sexo (m,f,0 1) de uma pessoa e calcule o seu IMC informando a situação da pessoa
 
-int genero;
+int genero, i = 0;
 double altura, peso, calculo;
 string mensagem, n1, n2 , n3, n4, n5, sair;
+
 
 sair = "N";
 
 while (sair != "S" && sair != "s")
 {
-    n1 = "";
-    n2 = "";
-    n3 = "";
-    n4 = "";
-    n5 = "";
-    mensagem = "";
+    //n1 = "";
+    //n2 = "";
+    //n3 = "";
+    //n4 = "";
+    //n5 = "";
+    //mensagem = "";
+    string[] msgfinal = new string[30];
     Console.WriteLine("Olá! Por favor, informe se você é:\n0 - Homem\n1 - Mulher");
     genero = int.Parse(Console.ReadLine());
 
@@ -43,6 +45,39 @@ while (sair != "S" && sair != "s")
 
     calculo = ((peso) / (altura * altura));
     mensagem = $" <--- Você está aqui (seu IMC: {calculo})";
+
+    switch (calculo)
+    {
+        case <= 18.5:
+            msgfinal[1] = mensagem;
+            break;
+        case <= 24.9:
+            msgfinal[2] = mensagem;
+            break;
+        case <= 29.9:
+            msgfinal[3] = mensagem;
+            break;
+        case <= 39.9:
+            msgfinal[4] = mensagem;
+            break;
+        default:
+            msgfinal[5] = mensagem;
+            break;
+    }
+
+    switch (genero)
+    {
+        case 0:
+            Console.WriteLine("Você é do gênero: Masculino");
+            break;
+        case 1:
+            Console.WriteLine("Você é do gênero: Feminino");
+            break;
+        default:
+            Console.WriteLine("Escolha uma das opções.");
+            break;
+    }
+    /*
     if (calculo <= 18.5)
     {
         n1 = mensagem;
@@ -72,13 +107,15 @@ while (sair != "S" && sair != "s")
     {
         Console.WriteLine("Você é do gênero: Feminino");
     }
+    */
+
     Console.WriteLine("------------------------------------------------------------------");
     Console.WriteLine("             IMC            | CLASSIFICAÇÃO     | OBESIDADE (GRAU)");
-    Console.WriteLine($"MENOR QUE   18,5            | MAGREZA           |   0   {n1}");
-    Console.WriteLine($"ENTRE       18,5 E 24,9     | NORMAL            |   0   {n2}");
-    Console.WriteLine($"ENTRE       25,0 E 29,9     | SOBREPESO         |   I   {n3}");
-    Console.WriteLine($"ENTRE       30,0 E 39,9     | OBESIDADE         |   II  {n4}");
-    Console.WriteLine($"MAIOR QUE   40,0            | OBESIDADE GRAVE   |   III {n5}");
+    Console.WriteLine($"MENOR QUE   18,5            | MAGREZA           |   0   {msgfinal[1]}");
+    Console.WriteLine($"ENTRE       18,5 E 24,9     | NORMAL            |   0   {msgfinal[2]}");
+    Console.WriteLine($"ENTRE       25,0 E 29,9     | SOBREPESO         |   I   {msgfinal[3]}");
+    Console.WriteLine($"ENTRE       30,0 E 39,9     | OBESIDADE         |   II  {msgfinal[4]}");
+    Console.WriteLine($"MAIOR QUE   40,0            | OBESIDADE GRAVE   |   III {msgfinal[5]}");
     Console.WriteLine("------------------------------------------------------------------");
 
     Console.WriteLine("Digite S para sair do programa ou aperte qualquer tecla para repetir.");
